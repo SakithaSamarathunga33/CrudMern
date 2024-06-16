8f1ae506dff4e904baaadfa629c1efd9cfb1e707
Sakitha Samarathunga <sakithaudarashmika@gmail.com>
2024-06-16 12:05:25 +0530

    cors add

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require ('body-parser');
+const cors = require('cors');
 
 // Express app
 const app = express()
@@ -10,6 +11,7 @@ const postRoutes = require('./routes/posts')
 
 //app middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 //routes middleware
 app.use(postRoutes);
