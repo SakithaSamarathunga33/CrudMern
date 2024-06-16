c95a407c76458fa34657c30b17c4619469ff33af
Sakitha Samarathunga <sakithaudarashmika@gmail.com>
2024-06-16 17:55:57 +0530

    interface admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
 import axios from 'axios';
 
@@ -27,19 +28,37 @@ componentDidMount(){
   }
   render() {
     return (
-      <div>
+      <div className="container">
         <p>All posts</p>
        <table class="table">
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">#</th>
-            <th scope="col">#</th>
-            <th scope="col">#</th>
-            <th scope="col">#</th>
-
+            <th scope="col">Topic</th>
+            <th scope="col">Description</th>
+            <th scope="col">Post Category</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
+        <tbody>
+          {this.state.posts.map((posts,index) => (
+            <tr>
+              <th scope="row">{index+1}</th>
+              <td>{posts.topic}</td>
+              <td>{posts.description}</td>
+              <td>{posts.postCategory}</td>
+              <td>
+                <a class="btn btn-warning" href="#">
+                  <i className="fas fa-edit"></i>&nbsp;Edit
+                </a>
+                &nbsp;
+                <a className="btn btn-danger" href='#'>
+                  <i className="far fa-trash-alt"></i>&nbsp;Delete
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
        </table>
 
         
