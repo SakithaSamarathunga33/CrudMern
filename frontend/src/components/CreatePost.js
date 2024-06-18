f0d05d96c77f1a0b10447ddd387d2f8b4d89037d
Sakitha Samarathunga <sakithaudarashmika@gmail.com>
2024-06-18 14:16:10 +0530

    input done

--- frontend/src/components/CreatePost.js
+++ frontend/src/components/CreatePost.js
@@ -1,41 +1,91 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class CreatePost extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      topic:"",
+      description:"",
+      postCategory:""
+    }
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+  
+
+  onSubmit= (e) =>{
+    e.preventDefault();
+
+    const {topic,description,postCategory} = this.state;
+
+    const data ={
+      topic:topic,
+      description: description,
+      postCategory: postCategory,
+    }
+    console.log(data)
+
+    axios.post("/posts/save",data).then((res) =>{
+      if(res.data.success){
+        this.setState(
+          {
+            topic:"",
+            description:"",
+            postCategory:"" 
+          }
+        )
+      }
+    })
+  }
+
   render() {
     return (
-      <div className="container mt-5 mb-5 d-flex justify-content-center">
-        <div className="card px-1 py-4">
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input className="form-control" type="text" placeholder="topic" />
+      <div className="col-md-8 mt-4 mx-auto">
+        <h1 className="h3 mb-3 font-weight-normal">Create New Post</h1>
+              <form className="needs-validation" noValidate>
+                <div className="form-group" style={{marginBottom:'15px'}}>
+                  <label style={{marginBottom:'5px'}}>Topic</label>
+                  <input className="form-control" 
+                   type="text" 
+                   name="topic" 
+                   placeholder="Enter topic" 
+                   value={this.state.topic} 
+                   onChange={this.handleInputChange} />
                 </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="form-group">
-                  <div className="input-group">
-                    <input className="form-control" type="text" placeholder="description" />
-                  </div>
+
+                <div className="form-group" style={{marginBottom:'15px'}}>
+                  <label style={{marginBottom:'5px'}}>Description</label>
+                  <input className="form-control" 
+                   type="text" 
+                   name="description"
+                   placeholder="Enter description" 
+                   value={this.state.description} 
+                   onChange={this.handleInputChange} />
                 </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="form-group">
-                  <div className="input-group">
-                    <input className="form-control" type="text" placeholder=" postCategory" />
-                  </div>
+
+                <div className="form-group" style={{marginBottom:'15px'}}>
+                  <label style={{marginBottom:'5px'}}>PostCategory</label>
+                  <input className="form-control" 
+                   type="text" 
+                   name="postCategory"
+                   placeholder="Enter postCategory" 
+                   value={this.state.postCategory}  
+                   onChange={this.handleInputChange} />
                 </div>
-              </div>
+
+                <button className="btn btn-success" type="submit" style={{marginTop:'15px'}} onClick={this.onSubmit}>
+                  <i className="far fa-check-square"></i>
+                  &nbsp; Save
+                  </button>
+              </form>
             </div>
-            <button className="btn btn-primary btn-block confirm-button">Confirm</button>
-          </div>
-        </div>
-      
     );
   }
 }
